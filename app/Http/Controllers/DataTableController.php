2690671c7038c90a7a4693ca60da51f71cab5d6e
micael-ricardo <micael.ricardo@outlook.com>
2023-08-24 06:14:40 -0300

    Alterar paginação seguindo doc datatable + filtro + orderby

--- app/Http/Controllers/DataTableController.php
+++ app/Http/Controllers/DataTableController.php
@@ -3,21 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pessoa;
+
 class DataTableController extends Controller
 {
     public function listar()
     {
         $draw = request()->input('draw');
-        $page = request()->input('start') / request()->input('length') + 1;
+        $start = request()->input('start');
+        $length =  request()->input('length');
+        $searchValue =  request()->input('search.value');
+        $orderColumn =  request()->input('columns')[request()->input('order')[0]['column']]['data'];
+        $orderDirection =  request()->input('order')[0]['dir'];
 
-        $model = Pessoa::with('enderecos.cidade')->paginate(5, ['*'], 'page', $page);
+        $query = Pessoa::with('enderecos.cidade');
+
+        // filtro 
+        if (!empty($searchValue)) {
+            $query->where(function ($query) use ($searchValue) {
+                $query->where('nome', 'like', '%' . $searchValue . '%')
+                    ->orWhere('idade', 'like', '%' . $searchValue . '%')
+                    ->orWhere('email', 'like', '%' . $searchValue . '%')
+                    ->orWhere('sexo', 'like', '%' . $searchValue . '%')
+                    ->orWhereHas('enderecos', function ($query) use ($searchValue) {
+                        $query->where('logradouro', 'like', '%' . $searchValue . '%')
+                            ->orWhere('bairro', 'like', '%' . $searchValue . '%')
+                            ->orWhereHas('cidade', function ($query) use ($searchValue) {
+                                $query->where('nome', 'like', '%' . $searchValue . '%');
+                            });
+                    });
+            });
+        }
+        // total de registros antes da paginação
+        $totalRecords = $query->count();
+        // ordenação
+        $query->orderBy($orderColumn, $orderDirection);
+        // Aplicar paginação
+        $query->skip($start)->take($length);
+        $model = $query->get();
+
         $response = [
             'draw' => $draw,
-            'recordsTotal' => $model->total(),
-            'recordsFiltered' => $model->total(),
-            'data' => $model->items(),
+            'recordsTotal' => $totalRecords,
+            'recordsFiltered' => $totalRecords,
+            'data' => $model,
         ];
-
         return response()->json($response);
     }
 }
